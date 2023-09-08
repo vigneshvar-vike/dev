d324ecd73c8adaae5634cce8c2aeea5b18c102cf
vigneshvar-vike <143002689+vigneshvar-vike@users.noreply.github.com>
2023-09-08 12:17:28 +0530

    Add files via upload

---
+++ jsfnew/src/main/java/com/alm/jsfnew/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.alm.jsfnew;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
